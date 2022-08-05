c50f1d4085b207b6e5bae13d7a0b4c74d03f01f5
kohai-fred <frederick.vu.pro@gmail.com>
2022-08-05 16:16:01 +0200

    Feat/alert design done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removePokemon, showModal } from "../actions";
 import styles from "./Modal.module.css";
@@ -8,11 +9,18 @@ const Modal = () => {
     const dispatch = useDispatch();
 
     const handleValidate = () => {
-        // useDispatch(removePokemon())
-        console.log("POKE VALIDATE", pokemon);
         dispatch(showModal(false));
         dispatch(removePokemon(pokemon));
     };
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        if (isOpen) {
+            body.style.position = "fixed";
+        } else {
+            body.style.position = "relative";
+        }
+    }, [isOpen]);
 
     return (
         <>
@@ -20,13 +28,15 @@ const Modal = () => {
                 <></>
             ) : (
                 <article className={styles.modal}>
-                    <div>
-                        <h1>JE SUIS LA MODAL</h1>
+                    <section>
+                        <p>
+                            Êtes vous sûr de vouloir supprimer <span>{pokemon.name} ?</span>
+                        </p>
                         <div>
                             <button onClick={handleValidate}>Confirmer</button>
                             <button onClick={() => dispatch(showModal(false))}>Annuler</button>
                         </div>
-                    </div>
+                    </section>
                 </article>
             )}
         </>
