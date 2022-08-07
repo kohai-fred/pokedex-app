4f9b4833b16538683388b0b6d2b7fe4d2258be63
kohai-fred <frederick.vu.pro@gmail.com>
2022-08-07 19:23:00 +0200

    Feat/scroll top

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -10,8 +10,12 @@ const SIZE_LOAD = 20;
 const List = ({ baseList, filteredList, from }) => {
     const [list, setList] = useState(filteredList.slice(0, SIZE_LOAD));
     const [card, setCard] = useState(null);
+    const [isScrollTop, setIsScrollTop] = useState(false);
     const ul = useRef(null);
 
+    /**
+     * Scroll infini
+     */
     // Création d'un écouteur d'évènement sur la page
     const oberser = new IntersectionObserver((entry) => {
         const newList = loadNextPokemons(filteredList, list, SIZE_LOAD);
@@ -30,31 +34,64 @@ const List = ({ baseList, filteredList, from }) => {
         oberser.observe(card);
     }, [card]);
 
+    /**
+     * Scroll top
+     */
+    // Création d'écouteur d'évènement
+    const scrollTopObserver = new IntersectionObserver((entry) => {
+        const ulTop = Math.abs(entry[0].boundingClientRect.top);
+        const winHeight = window.innerHeight;
+        if (ulTop - winHeight > winHeight) {
+            setIsScrollTop(true);
+        } else {
+            setIsScrollTop(false);
+        }
+    });
+
+    const scrollTop = () => {
+        window.scrollTo({
+            top: "82px",
+            behavior: "smooth",
+        });
+    };
+    // On lance l'écouteur
+    useEffect(() => {
+        if (!ul) return;
+        scrollTopObserver.observe(ul.current);
+    }, [card]);
+
     return (
-        <main className={styles.list}>
-            <article>
-                <Searchbar filteredList={filteredList} baseList={baseList} from={from} />
-            </article>
+        <>
+            <main className={styles.list}>
+                <article>
+                    <Searchbar filteredList={filteredList} baseList={baseList} from={from} />
+                </article>
 
-            <article className={styles.list_wrapper}>
-                <h1>{from}</h1>
+                <article className={styles.list_wrapper}>
+                    <h1>{from}</h1>
 
-                <ul className={styles.list_container} ref={ul}>
-                    {filteredList.length === 0 ? (
-                        <li className={styles.no_result}>
-                            <span>.</span>
-                            <span>.</span>
-                            <span>.</span>
-                            <span>?</span>
-                        </li>
-                    ) : (
-                        list.map((pokemon) => {
-                            return <Card key={pokemon.id} pokemon={pokemon} />;
-                        })
-                    )}
-                </ul>
-            </article>
-        </main>
+                    <ul className={styles.list_container} ref={ul}>
+                        {filteredList.length === 0 ? (
+                            <li className={styles.no_result}>
+                                <span>.</span>
+                                <span>.</span>
+                                <span>.</span>
+                                <span>?</span>
+                            </li>
+                        ) : (
+                            list.map((pokemon) => {
+                                return <Card key={pokemon.id} pokemon={pokemon} />;
+                            })
+                        )}
+                    </ul>
+                </article>
+            </main>
+            {isScrollTop && (
+                <div className={styles.scrollTop} onClick={scrollTop}>
+                    <div></div>
+                </div>
+            )}
+        </>
     );
 };
 
